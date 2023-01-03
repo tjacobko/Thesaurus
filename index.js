c3e227e0ebc0060f34a9705c6280309460ab1c3a
Jacob Ko <tko003@ucr.edu>
2023-01-03 01:41:21 -0800

    Added 'Enter' key to submit user input

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ search.addEventListener("click", function() {
     synonyms.innerHTML = str
 })
 
+// EventListener allows user to use 'Enter' to input word
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        search.click()
+    }
+})
+
+// Helper function to check if word
+// only contains alphas and spaces
 function isAlpha(str) {
     return /^[a-zA-Z ]+$/.test(str)
 }
